f5f0edd75fbf9c371f1498abd06470051c124421
Nicola Bellotti <n.bellotti90@gmail.com>
2016-11-12 02:44:46 +0100

    config iniziale

--- server/server.js
+++ server/server.js
@@ -47,6 +47,7 @@ app.get('/',function(req, res){
   res.sendFile(path.join(__dirname,"..","build","index.html"));
 });
 
+console.log(CONFIG);
 /////////////////////////////////////////////////////////////////////////////////
 //START SERVER
 app.listen(CONFIG.port, function(){
